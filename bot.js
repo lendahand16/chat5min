652fa4b3f4345638e8ae0296d0a7daad653ddc4d
Oliver Lenehan <oliver@mytanic.net>
2019-02-03 10:34:38 +1100

    rewrite so no adding channels. requires chat5min channel

--- bot.js
+++ bot.js
@@ -1,62 +1,30 @@
+//
+// (C) 2019 Oliver -- Lendahand16
+// Chat5Min Discord Chat Purge Bot
+//
+
 const Discord = require("discord.js");
 const Fs = require("fs");
 const client = new Discord.Client();
-//const PREFIX = "d$";
-const ADD_CHANNEL = "d$+";
-const REMOVE_CHANNEL = "d$-";
-const MSG_ADDED = "```css\nAdded this channel to the Chat5Min list```";
-const MSG_REMOVED = "```css\nRemoved Channel.```";
-const MSG_ALREADYREMOVED = "```css\nChannel not on list.```";
-//const MSG_CHANNELLIMIT = "```Sorry, you can only have one channel per guild.```";
 
-let guildChannels = [];
-
-client.on("ready", function () {
-    console.log("Chat5Min Ready!");
-    client.user.setPresence({"status":"online","game":{"name":"Delete Every 5 Minutes", "type":"LISTENING"}});
-});
-
-client.on("error", (err)=>{ console.log(err); });
-
-client.on("warn",(info)=>{ console.log(info); });
-
-client.on("message", function (msg) {
-    if (msg.author.id !== client.user.id && (msg.channel.type === "text")) {
-        if (msg.content === ADD_CHANNEL) {
-            if (sendPermsReminder(msg)) return;
-            let index = guildChannels.indexOf(msg.channel.id);
-            if (index > -1) {
-                //guildChannels.splice(index,1);
-                msg.channel.send("Channel already added.");
-            } else {
-                guildChannels.push(msg.channel.id);
-                deleteMsg(msg.channel).then(v=>{
-                    msg.channel.send(MSG_ADDED);
-                    msg.channel.send("```css\nThis is a #Chat5Min Channel. Messages will delete every 5 minutes.```");
-                });
-            }
-        } else if (msg.content === REMOVE_CHANNEL) {
-            if (sendPermsReminder(msg)) return;
-            let index = guildChannels.indexOf(msg.channel.id);
-            if (index > -1) {
-                guildChannels.splice(index,1);
-                msg.channel.send(MSG_REMOVED);
-            } else {
-                msg.channel.send(MSG_ALREADYREMOVED);
-            }
-        }
-    }
-});
-
-function sendPermsReminder(msg=Discord.Message.prototype) {
-    if (!msg.channel.permissionsFor(client.user).has(75776)) {
-        msg.author.send("```css\n[Missing required permissions] on \""+msg.guild.name+"\" #"+msg.channel.name+"```");
-        return true;
-    }
-    return false;
+function helpHandler(msg=Discord.Message.prototype) {
+    msg.author.send("This bot deletes messages every 5 minutes.\n\nSteps to using the Chat5Min bot.\n`[1]` Create a text channel called **`chat5min`**.\n`[2]` Give the following permissions to the Chat5Min bot in that channel:\n`Read Messages`,   `Send Messages`,   `Manage Messages`,   `Read Message History`.");
 }
 
-async function deleteMsg(channel=Discord.TextChannel.prototype) {
+function inputHandler(msg=Discord.Message.prototype) {
+    // check to see if message is from bot and is also in a guild (not a direct message).
+    if (!(msg.author.id !== client.user.id && (msg.channel.type === "text"))) return;
+    switch (msg.content) {
+        case "d$?":
+        case "d$help":
+            helpHandler(msg);
+        break;
+        default:
+        break;
+    }
+}
+
+function deleteMessages(channel=Discord.TextChannel.prototype) {
     return new Promise(async(resolve)=>{
         let currentMessages = await channel.fetchMessages();
         while (currentMessages.array().length > 0) {
@@ -67,31 +35,38 @@ async function deleteMsg(channel=Discord.TextChannel.prototype) {
     });
 }
 
-async function deleteChat() {
-    //console.log("HIII");
+function purgeChannels() {
     let currentMinutes = new Date().getMinutes();
-    if ((currentMinutes + 1) % 5 === 0) {
-        //console.log("TIK");
-        for (let chann of guildChannels) {
-            let currentChannel = client.channels.get(chann);
-            if (currentChannel) currentChannel.send("```Deleting in 1 minute.```");
-        }
-    } else if ((currentMinutes) % 5 === 0) {
-        //console.log("DEL");
-        for (let chann of guildChannels) {
-            let currentChannel = client.channels.get(chann);
-			if (currentChannel) {
-				deleteMsg(currentChannel).then(v=>{
-					currentChannel.send("```css\n#Chat5Min Channel. Messages will delete every 5 minutes.```");
-				});
-			}
+    for (let guild of client.guilds) {
+        const currentChannel = guild[1].channels.find(channel => channel.name === "chat5min");
+        if (currentChannel.permissionsFor(client.user).has(75776)) {
+            // Warning Message
+            if ((currentMinutes + 1) % 5 === 0)  {
+                currentChannel.send("Deleting all messages in 1 minute.");
+            // Delete Messages
+            } else if ((currentMinutes) % 5 === 0) {
+                deleteMessages(currentChannel).then(v=>{
+                    currentChannel.send("```css\n#Chat5Min Channel. Messages will delete every 5 minutes.```");
+                });
+            }
+        } else if (currentChannel.permissionsFor(client.user).has(2048)) {
+            currentChannel.send("Hey there, I need some help to work properly...\nHere's how to use the Chat5Min bot.\n`[1]` Create a text channel called **`chat5min`**.\n`[2]` Give the following permissions to the Chat5Min bot in that channel:\n`Read Messages`,   `Send Messages`,   `Manage Messages`,   `Read Message History`.");
         }
     }
 }
 
-if (Fs.existsSync("./token.txt")) {
-    client.login(Fs.readFileSync("./token.txt").toString());
-} else {
-    client.login(process.env.BOT_TOKEN);
+// Main Program
+
+client.on("ready",initBot);
+client.on("error",console.log);
+client.on("message", inputHandler);
+
+if (Fs.existsSync("./token.txt")) client.login(Fs.readFileSync("./token.txt").toString());
+else client.login(process.env.BOT_TOKEN);
+
+function initBot() {
+    console.log("Chat5Min Ready!");
+    setInterval(purgeChannels,1000*60);
+    client.user.setPresence({"status":"online","game":{"name":"d$?", "type":"LISTENING"}});
+    purgeChannels();
 }
-setInterval(deleteChat,1000*60);
