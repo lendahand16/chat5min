845e5d25e0042d93caaa1f2a64622d03b485aae0
Oliver Lenehan <oliver@mytanic.net>
2019-02-03 19:35:33 +1100

    bugpatch

--- bot.js
+++ bot.js
@@ -41,7 +41,7 @@ function purgeChannels() {
     let channelAmount = 0;
     for (let guild of client.guilds) {
         const currentChannel = guild[1].channels.find(channel => channel.name === "chat5min");
-        if (!currentChannel) return;
+        if (!currentChannel) break;
         if (currentChannel.permissionsFor(client.user).has(75776)) {
             // Warning Message
             if ((currentMinutes + 1) % 5 === 0)  {
